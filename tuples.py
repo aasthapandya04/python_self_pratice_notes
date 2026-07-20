66e5e1321f8c7a6360338b1b25f2c89f2178d422
aasthapandya04 <aastha0405pandya@gmail.com>
2026-07-20 22:16:17 +0530

    Tuples notes added

---
+++ tuples.py
@@ -0,0 +1,18 @@
+# Tuples 
+# Tuples can be changed after creation. IMMUTABLE 
+
+tup = (96, 33, 26, 45, 67, 89, 12, 34, 56, 78)
+print(type(tup))
+print(tup[5])
+
+# Accessing elements in a tuple
+
+print(tup[0:5])  # Slicing
+print(tup[-6])   # negative indexing
+print(tup[3])    #positive indexing
+print(tup[4:9:2])
+print(tup[2:])   # Slicing from index 2 to end
+print(tup[:7])
+
+# Methods in tuples
+
