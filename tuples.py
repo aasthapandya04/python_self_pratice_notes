c4d9a146b426798fc86ca880009554859812b3a8
aasthapandya04 <aastha0405pandya@gmail.com>
2026-07-22 22:34:35 +0530

    Added methods in tuples

--- tuples.py
+++ tuples.py
@@ -14,5 +14,28 @@ print(tup[4:9:2])
 print(tup[2:])   # Slicing from index 2 to end
 print(tup[:7])
 
-# Methods in tuples
+# Manipulation of tuples 
 
+countries = ('India', 'USA', 'UK', 'Canada', 'Australia')
+temp = list(countries)
+temp.append("Russia")
+temp.pop(3)
+temp[2] = "Finland"
+countries = tuple(temp)
+print(countries)
+
+# Methods in Tuples
+
+tuple1 = (1, 2, 3, 2, 4, 3, 2, 1, 1, 3, 2, 3,1,1)
+res = list(tuple1)
+res.append(5)
+print(tuple(res))
+
+result = tuple1.count(3)
+print("The count of 3 in this tuple is:" , result )
+
+result1 = tuple1.index(3)
+print("The first occurence of 3 is :" , result1)
+
+result2 = tuple1.index(3 , 4 , 8 )
+print("From the given range first occurence of 3 is " , result2 )
